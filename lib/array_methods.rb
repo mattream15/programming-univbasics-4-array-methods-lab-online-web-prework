c61bd5cad392e3d52945e7d4ea426ca37a438f20
generous-assembler-9561 <matthewtimothyream@gmail.com>
2019-08-07 15:27:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,10 +10,12 @@ end
 
 def using_reverse(array)
 using_reverse(array) = ["red", "yellow", "blue"]
+using_reverse(array).reverse
 end
 
 def using_first(array)
-
+using_first(array) = ["red", "yellow", "blue"]
+using_first(array).first
 end
 
 def using_last(array)
